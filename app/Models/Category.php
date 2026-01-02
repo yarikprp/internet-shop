410fc21de546d5b3dde1e1c581ac73cdc45bb60f
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2026-01-02 23:24:56 +0300

    Реализовал первичную структуру

---
+++ app/Models/Category.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\CategoryFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Category extends Model
+{
+    /** @use HasFactory<CategoryFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title'
+    ];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function (Category $category) {
+            $category->slug = $category->slug ?? str($category->title)->slug();
+        });
+    }
+
+
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,29 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Brand::factory(20)->create();
+
+
+        Product::factory(20)
+            ->has(Category::factory()->count(rand(1, 3)))
+            ->create();
     }
 }
